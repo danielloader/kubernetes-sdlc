0351c86546836b0249152a195b29ad18cde1eb2e
Daniel <hello@danielloader.uk>
2023-01-13 13:26:29 +0000

    fix: script

--- components/images/kafka-emitter/entrypoint.sh
+++ components/images/kafka-emitter/entrypoint.sh
@@ -6,7 +6,7 @@ KAFKA_BROKER=${KAFKA_BROKER:-kafka:9092}
 KAFKA_TOPIC=${KAFKA_TOPIC:-test}
 
 function send_kafka_messages() {
-  kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}" testfile.txt
+  kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}" << testfile.txt
 }
 
 
