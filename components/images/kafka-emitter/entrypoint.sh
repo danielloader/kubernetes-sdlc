dfeedfebaabdd6c803bf6c7c13d1f0a82f74458e
Daniel <hello@danielloader.uk>
2023-01-13 15:31:53 +0000

    feat: working kafka emitter

--- components/images/kafka-emitter/entrypoint.sh
+++ components/images/kafka-emitter/entrypoint.sh
@@ -4,13 +4,22 @@
 
 KAFKA_BROKER=${KAFKA_BROKER:-kafka:9092}
 KAFKA_TOPIC=${KAFKA_TOPIC:-test}
+LINE_COUNT=$(cat testfile.txt | wc -l)
+RECORDS_PER_SECOND=${RECORDS_PER_SECOND:-1000}
+VERBOSE=${VERBOSE:-false}
 
-function send_kafka_messages() {
-  kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}" << testfile.txt
+echo "STARTING"
+echo "Emitting ${RECORDS_PER_SECOND} records/s to ${KAFKA_BROKER}/${KAFKA_TOPIC} using a ${LINE_COUNT} length source file"
+
+function emit_lines() {
+  while true
+  do
+    cat testfile.txt 
+  done
 }
 
-
-while :
-do
-  send_kafka_messages
-done
+if [ "${VERBOSE}" == "true" ]; then
+  (emit_lines | stdbuf -eL pv --line-mode --rate-limit=${RECORDS_PER_SECOND} --bytes --rate --force --interval=1 --name="${KAFKA_BROKER}/${KAFKA_TOPIC} records" | stdbuf -eL pv --rate --bytes --force --name="Total Data" | kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}" ) 2>&1 | stdbuf -o0 tr '\r' '\n'
+else
+  emit_lines | pv --line-mode --rate-limit=${RECORDS_PER_SECOND} | kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}"
+fi
