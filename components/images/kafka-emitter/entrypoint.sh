268cf1afe7bb0f9af829c89d77699ee84b959c2f
Daniel <hello@danielloader.uk>
2023-01-13 13:25:24 +0000

    feat: add kafka-emitter

---
+++ components/images/kafka-emitter/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+ 
+# Globals
+
+KAFKA_BROKER=${KAFKA_BROKER:-kafka:9092}
+KAFKA_TOPIC=${KAFKA_TOPIC:-test}
+
+function send_kafka_messages() {
+  kafkacat -P -b "${KAFKA_BROKER}" -t "${KAFKA_TOPIC}" testfile.txt
+}
+
+
+while :
+do
+  send_kafka_messages
+done
